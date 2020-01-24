23f9980c4e3d93a7da3186cebc0f93326b03a932
kuba <jskop123@gmail.com>
2020-01-24 23:13:43 +0100

    mechanism editing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './css/style.css'
 
 let array = [
-  [2, 2, 4, 4],
-  [2, 2, 2, 4],
-  [0, 2, 0, 2],
+  [2, 4, 4, 4],
+  [4, 2, 2, 4],
+  [4, 2, 4, 2],
   [4, 4, 4, 4]
 ]
 
@@ -22,9 +22,9 @@ const removedZeros = (array) => {
   return arrayWithoutZeros
 }
   
-const isNumber = (item) => typeof item === 'number' ? true : false
+const isNumber = item => typeof item === 'number' ? true : false
 
-const summedElements = (array) => {
+const summedElementsLeft = (array) => {
   const sumElementsArray = array.map(element => {
     const answersArray = []
     for (let i = 0; i < 4; i++) {
@@ -41,12 +41,31 @@ const summedElements = (array) => {
   return sumElementsArray
 }
 
+const summedElementsRight = (array) => {
+  const sumElementsArray = array.map(element => {
+    const reversElement = element.reverse()
+    const answersArray = []
+    for (let i = 0; i < 4; i++) {
+      if (element[i] === element[i + 1] && element[i] !== 0) {
+        if (isNumber(element[i])) answersArray.push(element[i] += element[i + 1])
+        element.splice(element.indexOf(element[i + 1]), 1)
+      }
+      else {
+        if (isNumber(element[i])) answersArray.push(element[i])
+      }
+    }
+    return answersArray.reverse()
+  })
+  return sumElementsArray
+}
+
+
 const boxes = [...document.querySelectorAll('.box')]
 
 const reloadArray = (array) => {
   const iterableVar = array.flat(1)
   boxes.forEach((box, i) => {
-    if(iterableVar[i] !== '0'){
+    if(iterableVar[i] !== 0){
       box.innerHTML = iterableVar[i]
     }
     else{
@@ -54,9 +73,7 @@ const reloadArray = (array) => {
     }
   })
 }
-
 reloadArray(arrayCopy)
-
 
 const swipeLeft = (array) => {
   const swipeLeftArray = array.map(element => {
@@ -101,39 +118,50 @@ const swipeDown = (array) => {
   return transpose(swipeDownArray)
 }
 
+const addNumberAfterSwipe = (array) => {
+  const number = 2
+  for(let i=0; i<16; i++){
+    const rand1 = Math.round(Math.random()*3)
+    const rand2 = Math.round(Math.random()*3)
+    if(array[rand1][rand2]===0){
+      array[rand1][rand2] = number
+    }
+  }
+}
+
 window.addEventListener('keydown', event => {
   if(event.keyCode===37){
     const rmZeros = removedZeros(arrayCopy)
-    const sumEls = summedElements(rmZeros)
+    const sumEls = summedElementsLeft(rmZeros)
     const result = swipeLeft(sumEls)
     arrayCopy = result
     reloadArray(result)
+    //addNumberAfterSwipe(arrayCopy)
   }
   if(event.keyCode===39){
     const rmZeros = removedZeros(arrayCopy)
-    const sumEls = summedElements(rmZeros)
+    const sumEls = summedElementsRight(rmZeros)
     const result = swipeRight(sumEls)
     arrayCopy = result
     reloadArray(result)
+    //addNumberAfterSwipe(arrayCopy)
   }
   if(event.keyCode===38){
     const transposed = transpose(arrayCopy)
     const rmZeros = removedZeros(transposed)
-    const sumEls = summedElements(rmZeros)
+    const sumEls = summedElementsLeft(rmZeros)
     const result = swipeUp(sumEls)
     arrayCopy = result
     reloadArray(result)
+    //addNumberAfterSwipe(arrayCopy)
   }
   if(event.keyCode===40){
     const transposed = transpose(arrayCopy)
     const rmZeros = removedZeros(transposed)
-    const sumEls = summedElements(rmZeros)
+    const sumEls = summedElementsRight(rmZeros)
     const result = swipeDown(sumEls)
     arrayCopy = result
     reloadArray(result)
+    //addNumberAfterSwipe(arrayCopy)
   }
-  console.log(arrayCopy)
 })
-
-
-document.body.appendChild(component());
